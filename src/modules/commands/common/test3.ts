8a096d628a823c1d915060fdbe0c0bb3e765f9e1
planetsideapitest <imverygoodplayer@outlook.com>
2025-11-20 16:39:00 +0200

    Create test3.ts

---
+++ src/modules/commands/common/test3.ts
@@ -0,0 +1,13 @@
+import { randomChoice } from '@app/utils/random'
+import { Command } from '@commands/command-handler'
+
+export default new Command({
+  keyword: 'coinflip',
+  description: 'flip a coin',
+  help: 'Usage: `{prefix}coinflip` - flips a coin',
+  callback: ({ args, reply }) => {
+    if (args.length > 0) return
+
+    reply(randomChoice(['Heads!', 'Tails!']))
+  },
+})
